df3e6638e4605a82342ebe4ae6cc29c3c76f076a
hindulaatti <hindulaatti@gmail.com>
2021-12-31 00:26:20 +0200

    Add looking in different directions

--- camera.lua
+++ camera.lua
@@ -4,7 +4,7 @@
 --------
 --------------------------------------------------
 -- USER VARIABLES: Change these values depending on your preferences
-local baseFOV = 65 -- base vertical FOV
+local baseFOV = 45 -- base vertical FOV
 
 -- Changes the focus point of the camera 
 -- positive numbers towards the front and negative to the back of the car
@@ -16,6 +16,8 @@ local wheelCenterOffset = 0
 local wheelbase = 0
 local cgHeight = 0
 local initDone = false
+local lastForwardPositionInitDone = false
+local lastForwardPosition = vec3()
 
 function getWheelbase()
     local tyreFLPos = carPos - ac.getTyrePosition(1)
@@ -36,21 +38,41 @@ function getWheelCenterOffset(wheelCenterFlat)
     return wheelCenterFlat:length() * math.sign(-centerDir) + cameraYOffset
 end
 
-function getCameraDirection(camDir, pitch)
-    local direction = camDir * vec3(-1,-1,-1)
-    local pitchCorrection = vec3(0, pitch/50, 0)
+function getCameraDirection(camDir, pitch, cameraAngleModifer)
+    local direction = camDir * vec3(-1, -1, -1)
+    local camRight = math.cross(camDir, vec3(0,1,0)):normalize()
 
-    return direction + pitchCorrection
+    -- Apparently these rotate radians around the given vector, pretty useful
+    direction:rotate(quat.fromAngleAxis(math.radians(pitch), camRight))
+    direction:rotate(quat.fromAngleAxis(cameraAngleModifer, vec3(0,1,0)))
+
+    return direction
 end
 
-function getCameraPosition(camDir, carPos, heightCompensation, distance)
+function getCameraPosition(camDir, carPos, heightCompensation, distance, cameraAngleModifer)
+    ac.debug("cameraAngleModifer", cameraAngleModifer)
+
+    -- Rotate camera based on input
+    camDir:rotate(quat.fromAngleAxis(cameraAngleModifer, vec3(0,1,0)))
     local cameraPosition = carPos + camDir * distance
 
     return cameraPosition + heightCompensation
 end
 
+function getCameraAngleModifier()
+    local joystickLook = ac.getJoystickLook()
+    local lookDirection = (ac.looksLeft() and ac.looksRight() or 
+    ac.looksBehind()) and -1 or
+    ac.looksLeft() and 0.5 or
+    ac.looksRight() and -0.5 or
+    joystickLook ~= nil and joystickLook.x or 
+    0
+    return lookDirection * math.pi
+end
+
 function update(dt, cameraIndex)
     local cameraParameters = ac.getCameraParameters(cameraIndex)
+    local carDir = ac.getCarDirection()
     local carUp = ac.getCarUp()
     local acCarPos = ac.getCarPosition()
     
@@ -75,11 +97,19 @@ function update(dt, cameraIndex)
     local distance = cameraParameters.distance + (wheelbase) -- scale distance by wheelbase to compensate for different cars
 
     local heightCompensation = vec3(0, height, 0)
-    local cameraDir = (ac.Camera.position - carPos - heightCompensation):normalize()
 
-    --cameraDir = math.cross(cameraDir, vec3())
-    ac.Camera.direction = getCameraDirection(cameraDir, pitchAngle)
-    ac.Camera.position = getCameraPosition(cameraDir, carPos, heightCompensation, distance)
+    -- To stop this shit initializing to somewhere crap
+    if (not lastForwardPositionInitDone) then
+        lastForwardPosition = ac.Camera.position
+        lastForwardPositionInitDone = true
+    end
+    
+    local cameraDir = (lastForwardPosition - carPos - heightCompensation):normalize()
+    local cameraAngleModifier = getCameraAngleModifier()
+    
+    lastForwardPosition = getCameraPosition(cameraDir, carPos, heightCompensation, distance, 0)
+    ac.Camera.direction = getCameraDirection(cameraDir, pitchAngle, cameraAngleModifier)
+    ac.Camera.position = getCameraPosition(cameraDir, carPos, heightCompensation, distance, cameraAngleModifier)
 
     ac.Camera.up = vec3(0, 1, 0)
 
